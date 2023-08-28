222bbf636143fc17e6131299a01d4c7e5e03fab0
maxwellyue1 <140025346+maxwellyue1@users.noreply.github.com>
2023-08-28 09:18:53 -0230

    figure1, no birth no death, constan

--- figure1.py
+++ figure1.py
@@ -2,46 +2,49 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-stringencies = [0, 10, 50, 100, 200]
-parameters = {'beta_naught': 1.1, 'kappa': 1/6, 'gamma': 0.125, 'delta': 0.001, 'K': 0.4 }
 
-s0, e0, i0, r0 = 0.8, 0, 0.2, 0
+omega_const = 100
+stringencies = [100, 1000]
+parameters = {'pi':2e-6, 'mu':2e-6, 'beta_naught': 1.1, 'kappa': 1/6, 'gamma': 0.125, 'delta': 0.001, 'K': 0.4 }
+
+s0, e0, i0, r0 = 0.995, 0, 0.005, 0
+x0 = 0.9
 start_time = 0
-end_time = 300
-num_points = 301
+end_time = 600
+num_points = 601
 
-x = 4
+#x = 1
 
-seir0 = [s0, e0, i0, r0]  # Initial conditions
+seirx0 = [s0, e0, i0, r0, x0]  # Initial conditions
 t = np.linspace(start_time, end_time, num_points)
-
-def lambda_fct(i, x, parameters, stringency): 
-        lambdat = parameters['beta_naught'] * i / (1 + stringency * i * x)
-        return lambdat
 
 solutions_diff_stringencies = {}
 
 for stringency in stringencies: 
 
-    def seir_model(seir, t, parameters):
-        s, e, i, r = seir
-        lambdat = lambda_fct(i, x, parameters)
-        ds = - lambdat * s
-        de = lambdat * s - parameters['kappa'] * e
-        di = parameters['kappa'] * e - (parameters['gamma'] + parameters['delta']) * i
-        dr = parameters['gamma'] * i
+    def seir_model(seirx, t, parameters):
+        s, e, i, r, x = seirx
+        
+        lambdat = parameters['beta_naught'] * i / (1 + stringency * i * x)
+        ds = parameters['pi'] - (lambdat + parameters['mu']) * s
+        de = lambdat * s - (parameters['kappa'] + parameters['mu']) * e
+        di = parameters['kappa'] * e - (parameters['gamma'] + parameters['delta'] + parameters['mu']) * i
+        dr = parameters['gamma'] * i - parameters['mu'] * r
+        dx = parameters['K'] * x * (1-x) * (-1 + omega_const * i)
+        
 
-        return [ds, de, di, dr]
+        return [ds, de, di, dr, dx]
     
-    solution = odeint(seir_model, seir0, t, args=(parameters, ))
+    solution = odeint(seir_model, seirx0, t, args=(parameters, ))
     
     solutions_diff_stringencies[f'alpha={stringency}'] = solution
 
 for _ in solutions_diff_stringencies.keys(): 
-     plt.plot(t, solutions_diff_stringencies[_][:, 2])
-
+     plt.plot(t, solutions_diff_stringencies[_][:, 4], label = _)
+#plt.plot(t, solutions_diff_stringencies['alpha=10000'][:, 2])
 # Save the plot to a file
-plt.savefig('plot_example.png')
+plt.legend()
+plt.savefig('figure1.png')
 
 # Display the plot
 plt.show()
